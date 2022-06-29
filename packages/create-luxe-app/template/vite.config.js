3990235d676cf6069ab3e977c50c3a1166cf25b1
Tanner Lehett <john.cfplus@gmail.com>
2022-06-28 21:46:17 -0400

    chore(*): Get ready for alpha release .0

--- packages/create-luxe-app/template/vite.config.js
+++ packages/create-luxe-app/template/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
             '@assets': path.resolve(__dirname, './src/assets'),
             '@components': path.resolve(__dirname, './src/components'),
             '@contexts': path.resolve(__dirname, './src/contexts'),
+            '@hooks': path.resolve(__dirname, './src/hooks'),
             '@root': path.resolve(__dirname, './src/root'),
             '@services': path.resolve(__dirname, './src/services'),
             '@stylesheets': path.resolve(__dirname, './src/stylesheets'),
